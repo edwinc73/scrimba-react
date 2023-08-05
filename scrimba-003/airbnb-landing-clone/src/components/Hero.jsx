1096d7b40e4d96f3878e4efbaa6c39935ee9d4a9
Edwin Cheng <edwincheng09@googlemail.com>
2023-08-05 18:44:25 +0800

    nav done

---
+++ scrimba-003/airbnb-landing-clone/src/components/Hero.jsx
@@ -0,0 +1,21 @@
+import React from "react"
+import "./Hero.css"
+
+export default function Hero() {
+  return (
+    <>
+      <div>
+        <img src="../../images/image 22.jpg" alt="" />
+        <img src="../../images/image 23.jpg" alt="" />
+        <img src="../../images/image 24.jpg" alt="" />
+        <img src="../../images/image 25.jpg" alt="" />
+        <img src="../../images/image 26.jpg" alt="" />
+        <img src="../../images/image 27.jpg" alt="" />
+        <img src="../../images/image 28.jpg" alt="" />
+        <img src="../../images/image 29.jpg" alt="" />
+
+      </div>
+    </>
+  )
+}
+//
---
+++ scrimba-003/airbnb-landing-clone/src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import "./Navbar.css"
+
+export default function Navbar() {
+  return (
+    <>
+    <nav>
+      <img src="../../images/airbnb1.svg" alt="" />
+    </nav>
+
+    </>
+  )
+}
+//

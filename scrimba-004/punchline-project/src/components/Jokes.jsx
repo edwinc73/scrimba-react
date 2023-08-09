636be5b07b4672fead5ee08c8f1567f2a92fb57e
Edwin Cheng <edwincheng09@googlemail.com>
2023-08-09 16:20:15 +0800

    remove comment

--- scrimba-004/punchline-project/src/components/Jokes.jsx
+++ scrimba-004/punchline-project/src/components/Jokes.jsx
@@ -10,25 +10,3 @@ export default function Jokes(props) {
     </>
   )
 }
-
-
-
-/* Challenge:
-- App should render 4-5 <Joke /> components
-  (Joke component defined in its own file too)
-    - Each Joke should receive a "setup" prop and a "punchline" prop
-      and render those however you'd like
-- Use your favorite 2-part jokes (setup & punchline), or check
-  jokes.md file for some examples.
-
-
-
-EXTRA CREDIT:
-Some jokes are only a punchline with no setup:
-
-E.g.: "It’s hard to explain puns to kleptomaniacs because
-they always take things literally."
-
-If you don't pass in a "question" prop, how might you make it only
-show the punchline?
-*/

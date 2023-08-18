6e93086a66aeb6991998fe0042dc28992b66f72b
Edwin Cheng <edwincheng09@googlemail.com>
2023-08-18 10:40:46 +0800

    added inputs and onchange functions

--- scrimba-005/meme-generator/components/Main.jsx
+++ scrimba-005/meme-generator/components/Main.jsx
@@ -3,9 +3,50 @@ import React from "react"
 import "./Main.css"
 
 export default function Main(){
+  const url = "https://api.imgflip.com/get_memes"
+  const [imgUrl, setImgUrl] = React.useState("https://i.imgflip.com/1ur9b0.jpg")
+  const [top, setTop] = React.useState("")
+  const [bottom, setBottom] = React.useState("")
+
+  function createMeme(event) {
+    event.preventDefault()
+    fetchData()
+  }
+
+  function updateInput(e) {
+    if(e.target.name == "top"){
+      setTop(e.target.value)
+    } else {
+      setBottom(e.target.value)
+    }
+
+  }
+
+
+  function fetchData(){
+    fetch(url).then(
+      res => res.json()).then(
+        data => {
+          const randomNumber = Math.floor((Math.random() * 100))
+          const randomImg = data.data.memes[randomNumber]
+          setImgUrl(randomImg.url)
+        }
+      )
+  }
   return (
     <>
-
+    <main>
+      <form onSubmit={createMeme} className="form-container w-100">
+        <div className="d-flex input-container">
+          <input onChange={updateInput} type="text" className="top-text" placeholder="Top Text" name="top" />
+          <input onChange={updateInput} type="text" className="bottom-text" placeholder="Bottom Text" name="bottom" />
+        </div>
+      <button type="submit" className="w-100">Get a new meme image  🖼</button>
+      </form>
+      <div className="image-container">
+        <img src={imgUrl} alt="Meme image" className="meme-image" />
+      </div>
+    </main>
     </>
   )
 }
--- scrimba-005/meme-generator/components/Nav.jsx
+++ scrimba-005/meme-generator/components/Nav.jsx
@@ -7,7 +7,7 @@ export default function Nav(){
     <>
       <nav className="d-flex justify-content-between align-items-center nav-container">
         <div className="logo-container d-flex">
-          <img src="../images/Troll Face.svg" alt="Troll face" />
+          <img src="../images/TrollFace.svg" alt="Troll face" />
           <div className="logo-text">Meme Generator</div>
         </div>
         <div className="nav-title">React Course - Project 3</div>

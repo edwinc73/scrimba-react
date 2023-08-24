45b7b3d5ffa99ced0e147703787adccc621f8678
Edwin Cheng <edwincheng09@googlemail.com>
2023-08-24 15:31:10 +0800

    finsihed project, added checkbox, validations for pw and email

--- scrimba-006/sign-up-form/src/App.jsx
+++ scrimba-006/sign-up-form/src/App.jsx
@@ -1,13 +1,12 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Form from "../src/components/Form"
 
 function App() {
 
   return (
     <>
-
+    <Form />
     </>
   )
 }
--- scrimba-006/sign-up-form/src/components/Form.jsx
+++ scrimba-006/sign-up-form/src/components/Form.jsx
@@ -4,24 +4,75 @@ import "./Form.css"
 export default function Form(){
   const [formData, setFormData] = useState({
     email: "",
-    password: ""
-    newsletter
+    password: "",
+    newsletter: false
   })
-  return(
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        name="email"
-        value={email}
-        placeholder="Email address"
-      />
-        <input
-        type="text"
-        name="email"
-        value={email}
-        placeholder="Email address"
-      />
 
-    </form>
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    const regex = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/
+
+    if(!regex.test(formData.email)){
+      alert("Email is invalid")
+    }
+
+    if(e.target[2].value == formData.password){
+      console.log(formData)
+    } else {
+      alert("Password not the same")
+    }
+  }
+
+  const updateFormData = (e) =>{
+    setFormData(prev => {
+      const {name, type, value} = e.target
+      return (
+        type == "checkbox" ?
+        { ...prev, newsletter: !prev.newsletter } :
+        { ...prev, [name]: value }
+      )
+    })
+  }
+
+  const {email, password, newsletter} = formData
+  return(
+    <>
+      <div className="row form-container rounded">
+        <form onSubmit={handleSubmit} className="d-flex w-100 flex-column">
+          <input
+            className="rounded border border-secondary w-100"
+            type="text"
+            name="email"
+            value={email}
+            placeholder="Email address"
+            onChange={updateFormData}
+            />
+            <input
+              className="rounded border border-secondary w-100"
+            type="password"
+            value={password}
+            name="password"
+            placeholder="password"
+            onChange={updateFormData}
+            />
+            <input
+              className="rounded border border-secondary w-100"
+            type="password"
+            name="confirmPassword"
+            placeholder="Confirm Password"
+            />
+            <div className="d-flex w-100 newsletter">
+              <input
+              type="checkBox"
+              name="newsletter"
+              onChange={updateFormData}
+              checked={newsletter}
+              />
+               <label for="newsletter">I want to join the newsletter</label>
+            </div>
+            <button className="rounded btn btn-primary">Sign up</button>
+        </form>
+      </div>
+    </>
   )
 }

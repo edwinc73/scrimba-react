09c8769d81490a433d6cc8dc09680aba5430b96e
Edwin Cheng <edwincheng09@googlemail.com>
2023-09-01 14:35:02 +0800

    added timer format

--- scrimba-008/tenzies-game/src/App.jsx
+++ scrimba-008/tenzies-game/src/App.jsx
@@ -5,17 +5,12 @@ import Dice from "./components/Dice"
 function App() {
   const [die, setDie] = useState([])
   const [start, setStart] = useState(false)
-  const [timer, setTimer] = useState()
+  const [timer, setTimer] = useState(0)
+  const [win, setWin] = useState(false)
 
-  // useEffect(() => {
-  // }, [start])
-
-  const startTimer = () => {
-    const interval = setInterval(() => {
-      setTimer(prevTime => prevTime + 1)
-    }, 1000);
-    return interval;
-  }
+  useEffect(() => {
+    startTimer()
+  }, [start])
 
   useEffect(()=>{
     const arr = []
@@ -26,11 +21,34 @@ function App() {
   }, [])
 
   useEffect(() => {
-    win()
+    winCondition()
+    win && console.log("you win")
   }, [die])
+
+  // timer related js
+
+  const startTimer = () => {
+    if(start && timer == 0){
+      const interval = setInterval(() => {
+        setTimer(prevTime => prevTime + 1)
+      }, 1000);
+      return interval;
+    }
+  }
+
+  // change dice value related js
 
   const randomNumber = () => {
     return Math.floor(Math.random() * 10) + 1
+  }
+
+  const rollNumbers = () =>{
+    !start && startGame()
+    setDie(prev =>
+      prev.map(dice =>{
+        return dice.locked ? dice : {...dice, value: randomNumber()}
+      })
+    )
   }
 
   const handleClick = (e) =>{
@@ -42,18 +60,24 @@ function App() {
     })
   }
 
-  const rollNumbers = () =>{
-    setDie(prev =>
-      prev.map(dice =>{
-        return dice.locked ? dice : {...dice, value: randomNumber()}
-      })
-    )
+  // game  logic
+  const startGame = () => {setStart(true)}
+
+  const winCondition = () => {
+    if(die.length > 0 && die.every(dice => dice?.value === die[0].value)){
+      setWin(true)
+    }
   }
 
-  const win = () => {
-    if(die.length > 0 && die.every(dice => dice?.value === die[0].value)){
-      console.log("You Win!")
-    }
+  const intro = <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+
+  function formatTime(seconds) {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
   }
 
   return (
@@ -62,7 +86,8 @@ function App() {
         <div className="game-board">
           <div className="text-container">
             <h1>Tenzies</h1>
-            <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+            {start ? <p>{formatTime(timer)}</p>: intro}
+
           </div>
           <div className="die-container">
             {die.map(dice =>
